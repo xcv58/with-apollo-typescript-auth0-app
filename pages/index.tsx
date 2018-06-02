59deafe8c3c55d43334c26a62ed02b0c84786d6b
xcv58 <i@xcv58.com>
2018-06-01 22:43:36 -0700

    Add auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
+import Auth from './Auth'
 
-export default () => (
-  <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+const auth = new Auth()
+
+export default class App extends React.Component {
+  componentDidMount () {
+    auth.login()
+  }
+
+  render () {
+    return (
+      <ul>
+        <li><Link href='/a' as='/a'><a>a</a></Link></li>
+        <li><Link href='/b' as='/b'><a>b</a></Link></li>
+      </ul>
+    )
+  }
+}
