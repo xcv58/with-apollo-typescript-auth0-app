44792109b10046c663acf2f7f3bed131a3a8fc6d
xcv58 <i@xcv58.com>
2018-06-02 17:41:09 -0700

    Add jwt

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const next = require('next')
-const apolloServer = require('./apollo')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+// const jwtAuthz = require('express-jwt-authz')
+const cors = require('cors')
 const { parse } = require('url')
-import { resolvers as schema } from './apollo'
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -13,22 +15,27 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
-  // console.log({ apolloServer, app: server })
+
+  server.use(cors())
+  const checkJwt = jwt({
+    // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://xcv58.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer
+    audience: 'https://xcv58.auth0.com/api/v2/',
+    issuer: 'https://xcv58.auth0.com/',
+    algorithms: ['RS256']
+  })
 
   server.use(bodyParser.json())
-  // server.use(
-  //   session({
-  //     secret: 'super-secret-key',
-  //     resave: false,
-  //     saveUninitialized: false,
-  //     cookie: { maxAge: 60000 }
-  //   })
-  // );
-
-  // server.use('/api', apiRoutes);
-
-  // Server-side
-  // const route = pathMatch()
+  server.post('/timesheets', checkJwt, function(req, res) {
+    res.status(201).send({ message: 'This is the POST /timesheets endpoint' })
+  })
 
   server.get('/a', (req, res) => {
     return app.render(req, res, '/a', req.query)
